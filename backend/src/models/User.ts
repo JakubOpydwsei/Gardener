931762b11e0f216cfa799e1bd55c181aa9ac9af8
Hubert Rymarz <hubarym@wp.pl>
2026-01-18 07:05:53 +0100

    Seeding

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -16,7 +16,7 @@ const UserSchema = new Schema<User>({
         unique: true, 
         lowercase: true,
         trim: true,
-        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Wprowadzony email jest nieprawidłowy']
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Invalid email format']
  },
     passwordHash: { type: String, required: true },
     sessionToken: { type: String, unique: true, sparse: true },
